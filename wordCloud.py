cf8f2e8e9d4d5ae6612748fb27f9a8226a4c2953
Jon Brohauge <jonbrohauge@gmail.com>
2013-06-15 14:50:50 +0200

    Updated file to output in html

--- wordCloud.py
+++ wordCloud.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
-
 from operator import itemgetter
 import re
+import cgi
 
-inputListOfWords = 'hej, med, dig, ost, ost gedeost endda.!/ argh'
+form = cgi.FieldStorage()
+inputListOfWords = form.getfirst('wordCloudText', 'empty')
+
+# Avoid script injection escaping the user input
+inputListOfWords = cgi.escape(inputListOfWords)
+
+#inputListOfWords = 'hej, med, dig, ost, ost gedeost endda.!/ argh'
 wordDictionary = {}
 
 def buildDictionary(wordList):
 	p = re.compile('[a-zA-Z0-9]+')
-	print 'RegeX: ' + str(p.findall(wordList))
+#	print 'RegeX: ' + str(p.findall(wordList))
 	tmpDict = {}	
 	wordStringArray = p.findall(wordList)
 	for item in wordStringArray:
@@ -22,9 +28,17 @@ def buildDictionary(wordList):
 #--- RUNABLE ---#
 
 wordDictionary = buildDictionary(inputListOfWords)
-print('Input list of words: ' + inputListOfWords)
-print('Dictionary: %s' % wordDictionary)
-for key in sorted(wordDictionary, key=itemgetter(1), reverse=True):
-	print('Key: ' + str(key))
-	print('Value: ' + str(wordDictionary[key]))
+#print('Input list of words: ' + inputListOfWords)
+#print('Dictionary: %s' % wordDictionary)
 
+print """\
+Content-Type: text/html\n
+<html><body>
+<p>The submitted text gives this list of words:"</p>
+"""
+for key in sorted(wordDictionary, key=itemgetter(1), reverse=True):
+	print('Key: ' + str(key) + '&nbsp;--&gt;&nbsp; Value: ' + str(wordDictionary[key]) + '<br />\n')
+
+print """\
+</body></html>
+"""
