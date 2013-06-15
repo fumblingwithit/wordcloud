234b16d7b3de2db355faf35d796b60f9cdbd15c1
Jon Brohauge <jonbrohauge@gmail.com>
2013-06-15 16:01:44 +0200

    Trying to get sorting to work.

--- wordCloud.py
+++ wordCloud.py
@@ -4,47 +4,44 @@ import re
 import cgi
 
 form = cgi.FieldStorage()
+
 inputListOfWords = form.getfirst('wordCloudText', 'empty')
+inputListOfWords = (cgi.escape(inputListOfWords)).lower()
 
-# Avoid script injection escaping the user input
-inputListOfWords = cgi.escape(inputListOfWords)
-
-#inputListOfWords = 'hej, med, dig, ost, ost gedeost endda.!/ argh'
 wordDictionary = {}
 
 def buildDictionary(wordList):
-	p = re.compile('[a-zA-Z0-9]+')
-	tmpDict = {}	
-	wordStringArray = p.findall(wordList)
-	for item in wordStringArray:
-		if(tmpDict.has_key(item)):
-			newValue = tmpDict[item] + 1
-			tmpDict[item] = newValue
-		else:
-			tmpDict.update({item:1})
-	return tmpDict
+        p = re.compile('[a-zA-Z0-9]+')
+        tmpDict = {}
+        wordStringArray = p.findall(wordList)
+        for item in wordStringArray:
+                if(tmpDict.has_key(item)):
+                        newValue = tmpDict[item] + 1
+                        tmpDict[item] = newValue
+                else:
+                        tmpDict.update({item:1})
+        return tmpDict
 
 #--- RUNABLE ---#
 
 wordDictionary = buildDictionary(inputListOfWords)
+sortedDictionary = sorted(wordDictionary, key=itemgetter(1), reverse=True)
 
 print """\
 Content-Type: text/html\n
 <html><body>
 """
-
 print """\
-<p>Debugging: %s</p>
+<p>Rough list: %s</p>
 """ %wordDictionary
-
 print """\
 <p>The submitted text gives this list of words:</p>
 """
 
-sortedDictionary = sorted(wordDictionary, key=itemgetter(1), reverse=True)
 for key in sortedDictionary:
-	print('Key: ' + str(key) + '&nbsp;--&gt;&nbsp; Value: ' + str(wordDictionary[key]) + '<br />\n')
+        print('Key: ' + str(key) + '&nbsp;&gt;&nbsp; Value: ' + str(wordDictionary[key]) + '<br />\n')
 
 print """\
 </body></html>
 """
+
